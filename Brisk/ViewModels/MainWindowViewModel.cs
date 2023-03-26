5880cfc511950bcd3742ce755aa3cb12205cb097
leskovde <leskovar.denis@seznam.cz>
2023-03-26 23:13:40 +0200

    Updated status bar.

--- Brisk/ViewModels/MainWindowViewModel.cs
+++ Brisk/ViewModels/MainWindowViewModel.cs
@@ -1,6 +1,30 @@
-﻿namespace Brisk.ViewModels;
+﻿using Avalonia.Media;
+using ReactiveUI;
+
+namespace Brisk.ViewModels;
 
 public class MainWindowViewModel : ViewModelBase
 {
     public string CurrentFile => "new.txt";
+    public string Status => "Ready";
+    public IBrush StatusColor
+    {
+        get
+        {
+            if (Status == "Ready")
+                return Brushes.Green;
+            else if (Status == "Error")
+                return Brushes.Red;
+            else
+                return Brushes.Yellow;
+        }
+    }
+    public string CaretPosition => $"0:{CaretIndex}";
+
+    private int _caretIndex;
+    public int CaretIndex
+    {
+        get => _caretIndex;
+        set => this.RaiseAndSetIfChanged(ref _caretIndex, value);
+    }
 }
